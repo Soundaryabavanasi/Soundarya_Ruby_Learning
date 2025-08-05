fb1aaa5aad0c228abc920d44553deb277ad66072
Soundarya Bavanasi <bavanasisoundarya2001@gmail.com>
2025-08-05 12:18:37 +0530

    rebase testing

--- feature.rb
+++ feature.rb
@@ -1,1 +1,12 @@
 ## testing the bratch whether the bratch is able to working or not while pull request.
+
+## To switch into bratch and also fetch the latest updates from the main bratch since many people will update the gitup by pushing the updated need to get the updated from the main to our bratch so that we do this for example
+## i have a bratch called "main" in githup repositary i cloned it before pushing into the main repositary need to verify the changes by sending the pull request we need to create sub bratch to raise a pull request in the github the revieweres will review and merge the changes into main repositary.
+## we can also push directlty to main repositary by switching in the vscode to main.
+
+git checkout feature/your-feature-name. ## it is a bratch after checkout feature/here you can add your own bratch name
+git fetch origin   ## origin main bratch
+git rebase origin/main.  ## getting updates from the main bratch upto date
+
+
+## testing
