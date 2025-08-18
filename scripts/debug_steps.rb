1a591a75e750804022d9dbedc973f4bce6168a97
Soundarya Bavanasi <bavanasisoundarya2001@gmail.com>
2025-08-18 16:56:38 +0530

    testing

--- scripts/debug_steps.rb
+++ scripts/debug_steps.rb
@@ -15,4 +15,5 @@ main
 ## it moved to another method where the declared inside a method another method called and we can see the main method is called out so it goes into the method fist
 ## and check the another mwthod is called and we can see main method has the variable with value so this value will take to greet(user) variable and update the user variable value with name.
 
+## This will help us to debug step by step
 
